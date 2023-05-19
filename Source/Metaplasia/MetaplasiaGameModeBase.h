b35579ea436067d3c6b953669330779d856ee4a3
Zoheth <z0heth@outlook.com>
2023-05-19 11:16:25 +0800

    Initial commit

---
+++ Source/Metaplasia/MetaplasiaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetaplasiaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METAPLASIA_API AMetaplasiaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
